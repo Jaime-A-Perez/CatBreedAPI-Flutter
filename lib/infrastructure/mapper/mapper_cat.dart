84a28ab3e5d9e12610ee97199a3d5df65e408107
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-12 16:16:52 -0500

    Created Mapper that transforms from CatModel to Cat

---
+++ lib/infrastructure/mapper/mapper_cat.dart
@@ -0,0 +1,17 @@
+import 'package:test_pragma/domine/entities/cat.dart';
+import 'package:test_pragma/infrastructure/models/cat_model.dart';
+
+class MapperCat{
+  static Cat catModelToCatEntity(CatModel catModel){
+    return Cat(
+    name: catModel.name,
+    origin: catModel.origin,
+    intelligence: catModel.intelligence.toString(),
+    description: catModel.description,
+    lifeSpan: catModel.lifeSpan,
+    adaptability: catModel.adaptability.toString(),
+    urlImage:"https://cdn2.thecatapi.com/images/${catModel.referenceImageId}.jpg",
+    id: catModel.id);
+
+  }
+}
