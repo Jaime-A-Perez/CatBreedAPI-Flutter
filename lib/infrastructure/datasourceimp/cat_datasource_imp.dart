33e00e59b13c1582ab8605afe14d066a32de93b3
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-12 16:34:24 -0500

    Implementation of getCats function and correction of conditional statusCode

--- lib/infrastructure/datasourceimp/cat_datasource_imp.dart
+++ lib/infrastructure/datasourceimp/cat_datasource_imp.dart
@@ -1,6 +1,7 @@
 import 'package:test_pragma/config/constants/constants.dart';
 import 'package:test_pragma/domine/datasources/cat_datasource.dart';
 import 'package:test_pragma/domine/entities/cat.dart';
+import 'package:test_pragma/infrastructure/mapper/mapper_cat.dart';
 import 'package:test_pragma/infrastructure/models/cat_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -8,25 +9,26 @@ class CatDataSourceImp extends CatDataSource {
   Future<List<CatModel>> _response() async{
     final Uri uri = Uri(
         scheme: 'https',
-        host: ' api.thecatapi.com',
+        host: 'api.thecatapi.com',
         path: '/v1/breeds',
-        queryParameters:  {'limit': 10});
+        queryParameters:  {'limit': "10"});
 
     final response = await http.get(uri, headers: {'x-api-key': Constants.apiKey});
     
-    if (response.statusCode < 200 && response.statusCode > 299) {
+    if (response.statusCode < 200 || response.statusCode > 299) {
       throw Exception("Error code: ${response.statusCode}");
     }    
 
     final List<CatModel> catModel = catFromJson(response.body);
-    // final CatModel catModel =  CatModel.catFromJson(response.body);
     return catModel; 
   }
   
   @override
   Future<List<Cat>> getCats() async{
-
-    throw UnimplementedError();
+    final resultModel = await _response();
+    final List<Cat> cats = resultModel.map((CatModel catModel) => MapperCat.catModelToCatEntity(catModel)).toList();
+        // throw UnimplementedError();
+    return cats;
   }
 
 }
