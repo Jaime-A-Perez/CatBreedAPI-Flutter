3a79396a556a1bc7bbbfef034912faedefed9ce1
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-12 14:15:05 -0500

    Created the Cat entity

---
+++ lib/domine/entities/cat.dart
@@ -0,0 +1,21 @@
+class Cat {
+  final String name;
+  final String origin;
+  final String description;
+  final String intelligence;
+  final String lifeSpan;
+  final String adaptability;
+  final String referenceImageId;
+  final String id;
+
+  Cat({
+      required this.name,
+      required this.origin,
+      required this.intelligence,
+      required this.description,
+      required this.lifeSpan,
+      required this.adaptability,
+      required this.referenceImageId,
+      required this.id
+      });
+}
