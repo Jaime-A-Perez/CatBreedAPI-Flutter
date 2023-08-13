6752650df2f5463a564f7ed5dcdc8744c2c82410
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-12 21:45:11 -0500

    Se cambió LandingScreen a StatefulWidget y se realizó la implementación de ScrollController.

--- lib/view/screens/landing_screen.dart
+++ lib/view/screens/landing_screen.dart
@@ -3,29 +3,64 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:test_pragma/blocs/bloc/cat_bloc.dart';
 import 'package:test_pragma/view/widgets/card_widget.dart';
 
-class LandingScreen extends StatelessWidget {
+class LandingScreen extends StatefulWidget {
   const LandingScreen({super.key});
 
+  @override
+  State<LandingScreen> createState() => _LandingScreenState();
+}
+
+class _LandingScreenState extends State<LandingScreen> {
+  final ScrollController scrollController = ScrollController();
+  @override
+  void initState() {
+    scrollController.addListener(() {
+      if (scrollController.position.pixels + 300 >= 
+      scrollController.position.maxScrollExtent &&
+      scrollController.position.pixels  <= 
+      scrollController.position.maxScrollExtent
+      ){
+       final bloc = context.read<CatBloc>();
+       bloc.add(AddNewItems());
+      };
+     });
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CatBloc, CatBlocState>(
       builder: (context, state) {
         if (state.catList == null) {
-          return Container(
-          color: Colors.amber,
+          return const Center(          
           child: Text("no data"),
         );
         }else {
           return Scaffold(
-            body: CardCustom(
-              breed: '${(state.catList?[0].breed) }', 
-              countryOfOrigin: '${state.catList?[0].origin}', 
-              intelligence: '${state.catList?[0].intelligence}', 
-              image: '${state.catList?[0].urlImage}',   
+            body: SafeArea (
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: ListView.builder(
+                        controller: scrollController,
+                        itemCount: state.catList!.length,
+                        itemBuilder: (context, int i) {
+                          return CardCustom(
+                            breed: '${(state.catList?[i].breed) }', 
+                            countryOfOrigin: '${state.catList?[i].origin}', 
+                            intelligence: '${state.catList?[i].intelligence}', 
+                            image: '${state.catList?[i].urlImage}',   
+                          );
+                        }
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           );
-        }
-        
+        }        
       },
     );
   }
