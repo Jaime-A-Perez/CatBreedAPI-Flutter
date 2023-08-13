2f82da57a2aa9450e2f260734506ba32b749d44f
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-13 08:05:27 -0500

    Created the function _responseSearch in CatDataSourceImp

--- lib/infrastructure/datasourceimp/cat_datasource_imp.dart
+++ lib/infrastructure/datasourceimp/cat_datasource_imp.dart
@@ -6,11 +6,12 @@ import 'package:test_pragma/domine/entities/cat.dart';
 import 'package:test_pragma/infrastructure/mapper/mapper_cat.dart';
 import 'package:test_pragma/infrastructure/models/cat_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:test_pragma/infrastructure/models/search_model.dart';
 
 class CatDataSourceImp extends CatDataSource {
 
 
-  Future<List<CatModel>> _response(Map<String,String> queryParameters) async{
+  Future<List<CatModel>> _response(Map<String,String> queryParameters, ) async{
     final Uri uri = Uri(
         scheme: 'https',
         host: 'api.thecatapi.com',
@@ -34,15 +35,29 @@ class CatDataSourceImp extends CatDataSource {
         // throw UnimplementedError();
     return cats;
   }
-  
-  @override
-  Future<Cat> searchCat(String breed) async{
-    final resultSearch = await _response({'breed_ids': breed});
-    final List<Cat> cats = resultSearch.map((CatModel catModel) => MapperCat.catModelToCatEntity(catModel)).toList();
-    return cats.first;
 
-    throw UnimplementedError();
+  Future<List<Breed>> _responseSearch(Map<String,String> queryParameters, ) async{
+    final Uri uri = Uri(
+        scheme: 'https',
+        host: 'api.thecatapi.com',
+        path: '/v1/images/search',
+        queryParameters: queryParameters);
+
+    final response = await http.get(uri, headers: {'x-api-key': Constants.apiKey});
+    
+    if (response.statusCode < 200 || response.statusCode > 299) {
+      throw Exception("Error code: ${response.statusCode}");
+    }    
+
+    final List<Breed> breed = breedFromJson(response.body);
+    return breed; 
+  }
+   @override
+  Future<Cat?> searchCat(String breed) async{
+    final resultSearch = await _responseSearch({'breed_ids': breed});
+    final List<Cat?> cats = resultSearch.map((Breed breed) => MapperCat.catModelToCatEntity(breed.breeds!.first)).toList();
+    return cats.isEmpty ? null : cats.first;
+
+    
   }  
-
-
 }
