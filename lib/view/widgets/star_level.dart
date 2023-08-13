0f205e7308fd4973d5c41a7dab315e06aa534886
Jaime A Perez R <catecppcemp@gmail.com>
2023-08-12 23:45:17 -0500

    Created StarLevel widget

---
+++ lib/view/widgets/star_level.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+
+class StartLevel extends StatelessWidget {
+  final int stars;
+
+  const StartLevel({super.key, required this.stars});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: List.generate(5, (index) {
+        if (index < stars) {
+          return  Icon(Icons.star, color: theme.disabledColor);
+        } else {
+          return  Icon(Icons.star_border, color: theme.focusColor);
+        }
+      }),
+    );
+  }
+}
